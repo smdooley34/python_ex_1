9e2d42fedf9fb0af4fc8f33c77765cc52d3e837d
Sarah Dooley <sarahdooley@MacBook-Pro.local>
2021-06-02 14:37:24 -0600

    [Homework One/Exercise Two] Completes Exercise Two.

--- one ex per file/ex2.py
+++ one ex per file/ex2.py
@@ -1,45 +1,46 @@
-# part 2
-# Task: Break s into sentences (which end in a .), count them and print them out using a loop:
-# result: (I truncated them with ...)
-# there are 4 sentences:
-# Python is an interpreted, high-level, general-purpose programming language
-#  Created by Guido van Rossum and first released in 1991, Python ...
-#  Its language constructs and object-oriented approach aim to help programmers ...
-print("start of part 2") # set breakpoint here
-s = "Python is an interpreted, high-level, general-purpose programming language. Created by Guido van Rossum and first released in 1991, Python's design philosophy emphasizes code readability with its notable use of significant whitespace. Its language constructs and object-oriented approach aim to help programmers write clear, logical code for small and large-scale projects."
-# your code here
+####################################################################################################
+# ex2.py
+#
+# This file contains the solution to Homework One Exercise Two.
+#
+# Prompt:
+#
+#   Break an input sentence into sentences (which end in a .), count them, and print them out using a loop:
+#
+#     Input: Python is an interpreted, high-level, general-purpose programming language. Created by
+#            Guido van Rossum and first released in 1991, Python's design philosophy emphasizes code
+#            readability with its notable use of significant whitespace. Its language constructs and
+#            object-oriented approach aim to help programmers write clear, logical code for small and
+#            large-scale projects.
+#     Result: There are 3 sentences
+#             Python is an interpreted, high-level, ge...
+#             Created by Guido...
+#             Its language constructs and...
+#
+####################################################################################################
 
-print("end of 2") # set breakpoint here 
-'''
+print("----- Start Of Part 2 -----")
+print("\n")
 
+inputParagraph = "Python is an interpreted, high-level, general-purpose programming language. Created by Guido van" \
+                 "Rossum and first released in 1991, Python's design philosophy emphasizes code readability with" \
+                 "its notable use of significant whitespace. Its language constructs and object-oriented approach" \
+                 "aim to help programmers write clear, logical code for small and large-scale projects."
+print("Input Paragraph:", inputParagraph)
 
+# This Would Certainly Be The Way I Would Do This... Use A Period As The Delimiter In Distinguishing Sentences
+sentenceList = inputParagraph.split(".")
+# We Will, However, Be Left With An Empty String As The Last Member Of The `sentenceList` Array, So We Remove It
+sentenceList.pop()
 
+# We Print The Number of Sentences Detected...
+print("Number Of Sentences:", len(sentenceList))
 
+# ... And We Also Print Each Of The Detected Sentences
+index = 0
+while index < len(sentenceList):
+    print("Sentence ", index + 1, sentenceList[index] + ".")
+    index += 1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-s = "Python is an interpreted, high-level, general-purpose programming language."
-sentence_list = s.split('.')
-print("there are", len(sentence_list), "sentences:")
-for e in sentence_list:
-    print(e)
-'''
+print("\n")
+print("----- End Of Part 2 -----")
