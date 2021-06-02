415408f3109d054c4c112f702e569a26787d5ab3
Sarah Dooley <sarahdooley@MacBook-Pro.local>
2021-06-02 14:36:41 -0600

    [Homework One/Exercise One] Completes Exercise One.

--- one ex per file/ex1.py
+++ one ex per file/ex1.py
@@ -1,57 +1,35 @@
-# part 1
-# task:  L = [0, [], [1,2,3,4], [[5],[6,7]], [8,9,10]]
-# using both, indexing and slicing on L, assemble and print a new list N that contains:
-# [0,2,3,[5,6],8,10] 
-# as an example, here's  how i've constructed a similar list X which contains [[2, 3], [10]] from L:
+####################################################################################################
+# ex1.py
 #
-# L = [0, [], [1,2,3,4], [[5],[6,7]], [8,9,10]]
-# X = [ L[2][1:-1], L[-1][2] ] 
-# print(x)  => [[2, 3], [10]]
-# You need to do something similar but end up with [0,2,3,[5,6],8,10] instead. One way to work 
-# through this is to break the process down in small steps, store result of each step in a new variable
-# and use those variables in the next step
-print("start of part 1") # set breakpoint here
-L = [0, [], [1,2,3,4], [[5],[6,7]], [8,9,10]]
-print(L)
-# your code
+# This file contains the solution to Homework One Exercise One.
+#
+# Prompt:
+#
+#   L = [0, [], [1, 2, 3, 4], [[5], [6, 7]], [8, 9, 10]]
+#
+#   Using both indexing and slicing on L, assemble and print a new list N that contains:
+#     [0, 2, 3, [5, 6], 8, 10]
+#
+#   As an example, here's how I've constructed a similar list X which contains [[2, 3], [10]] from L:
+#
+#   L = [0, [], [1, 2, 3, 4], [[5], [6, 7]], [8, 9, 10]]
+#   X = [ L[2][1:-1], L[-1][2] ]
+#   print(x) => [[2, 3], [10]]
+#
+#   You need to do something similar but end up with [0, 2, 3, [5, 6], 8, 10] instead. One way to
+#   work through this is to break the process down in small steps, store result of each step in a
+#   new variable and use those variables in the next step.
+####################################################################################################
 
+print("----- Start Of Part 1 -----")
+print("\n")
 
-print("end of 1") # set breakpoint here 
-'''
+L = [0, [], [1, 2, 3, 4], [[5], [6, 7]], [8, 9, 10]]
+print("Old Array: ", L)
 
+# Simply Directly Accessing Required Indices Is Probably The Most Readable But Easily Unwieldy Approach
+N = [L[0], L[2][1], L[2][2], [L[3][0][0], L[3][1][0]], L[4][0], L[4][2]]
+print("New Array: ", N)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# solution 2
-L = [0, [], [1,2,3,4], [[5],[6,7]], [8,9,10]]
-print("L is", L)
-tmp1 = L[0] # 0
-tmp2 = L[2][1] # 2
-tmp3 = L[2][2] # 3
-tmp4 = [L[3][0][0], L[3][1][0]] # [5, 6]
-tmp5 = L[4][0] # 8
-tmp6 = L[4][-1] # 10 
-newL = [tmp1, tmp2, tmp3, tmp4, tmp5, tmp6] # create final list
-print(newL) # [0, 2, 3, [5, 6], 8, 10]
-'''
+print("\n")
+print("----- End Of Part 1 -----")
